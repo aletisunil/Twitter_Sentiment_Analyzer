fd0f2baf0f528a2d38e6b11f561e77da1d9ca166
Aleti_Sunil <aletisunil1997@gmail.com>
2020-08-20 16:54:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def app():
     consumerSecretKey=""    #confidential
     accessToken=""          #confidential
     accessTokenSecret=""    #confidential
-
     #creating an object for authentication
     authenticate=tweepy.OAuthHandler(consumerKey,consumerSecretKey)
     #For access
@@ -36,15 +35,17 @@ def app():
     st.sidebar.image("https://res.cloudinary.com/practicaldev/image/fetch/s--AhOnOSVL--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/340844/c6ddb46a-f369-44ab-bef9-d2d8f953f3ac.png",width=None)
     st.sidebar.title("Sunil Aleti")
     st.sidebar.text("Developer at Cognizant")
-    st.sidebar.text("Blogger at DEV Community")
     st.sidebar.text("Quite Optimistic about AI")
+    st.sidebar.text("Blogger at DEV Community")
+
 
     username=st.text_area("Enter Username without @")
     
     #selectBox
     activity=st.selectbox("Select Activity",["Fetch latest tweets","Generate Word Cloud","Visualize Sentiment Analysis"])
     if st.button("Submit"):
-        if(len(username.strip())==0):
+        username=username.strip().replace(" ","")
+        if(len(username)==0):
             st.error("username can't be empty 😒")
         else:
             posts=api.user_timeline(screen_name=username,count=100,lang="en",tweet_mode="extended")
@@ -99,65 +100,83 @@ def app():
             df["Analysis"]=df["Polarity"].apply(getAnalysis)
     
             if(activity=="Visualize Sentiment Analysis"):
-                st.success("Visualizing tweets...")
-                st.markdown("<h2 style='text-align: center;'>Tweets</h2>", unsafe_allow_html=True)
-                st.dataframe(df,width=None,height=None)
-                #st.header("Polarity & Subjectivity")
-                st.markdown("<h2 style='text-align: center;'>Polarity & Subjectivity</h2>", unsafe_allow_html=True)
-                sns.scatterplot(x="Polarity", y="Subjectivity",data=df)
-                st.pyplot()
+                if(len(df)>0):
+                    st.success("Visualizing tweets...")
+                    st.markdown("<h2 style='text-align: center;'>Tweets</h2>", unsafe_allow_html=True)
+                    st.dataframe(df,width=None,height=None)
+                    #st.header("Polarity & Subjectivity")
+                    st.markdown("<h2 style='text-align: center;'>Polarity & Subjectivity</h2>", unsafe_allow_html=True)
+                    sns.scatterplot(x="Polarity", y="Subjectivity",data=df)
+                    st.pyplot()
         
-                #Get % of positive tweets
-                ptweets= df[df.Analysis=="Positive"]
-                ptweets=ptweets["Tweets"]
+                    #Get % of positive tweets
+                    ptweets= df[df.Analysis=="Positive"]
+                    ptweets=ptweets["Tweets"]
         
-                st.subheader("% of Positive Tweets")
-                st.success(round((ptweets.shape[0]/df.shape[0])*100,1))
-                #Get % of negative tweets
-                ntweets= df[df.Analysis=="Negative"]
-                ntweets=ntweets["Tweets"]
-                st.subheader("% of Negative tweets")
-                st.success(round((ntweets.shape[0]/df.shape[0])*100,1))
+                    st.subheader("% of Positive Tweets")
+                    st.success(round((ptweets.shape[0]/df.shape[0])*100,1))
+                    #Get % of negative tweets
+                    ntweets= df[df.Analysis=="Negative"]
+                    ntweets=ntweets["Tweets"]
+                    st.subheader("% of Negative tweets")
+                    st.success(round((ntweets.shape[0]/df.shape[0])*100,1))
 
     
         
-                st.markdown("<h2 style='text-align: center;'>Sentiment Analysis</h2>", unsafe_allow_html=True)
-                sns.set(style="darkgrid")
-                sns.countplot(x="Analysis",hue="Analysis",data=df)
-                st.pyplot()
+                    st.markdown("<h2 style='text-align: center;'>Sentiment Analysis</h2>", unsafe_allow_html=True)
+                    sns.set(style="darkgrid")
+                    sns.countplot(x="Analysis",hue="Analysis",data=df)
+                    st.pyplot()
 
-                #Most Repeated Words in tweets
-                st.markdown("<h2 style='text-align: center;'>Most Frequent Words</h2>", unsafe_allow_html=True)
-                words=[]
-                for tweet in df2["tweets"]:
-                    for j in tweet.split():
-                        words.append(j)
-                freq = Counter(words).most_common(30)
-                freq = pd.DataFrame(freq)
-                freq.columns = ['word', 'frequency']
-                sns.barplot(y="word", x="frequency",data=freq)
-                st.pyplot()
+                    #Most Repeated Words in tweets
+                    st.markdown("<h2 style='text-align: center;'>Most Frequent Words</h2>", unsafe_allow_html=True)
+                    words=[]
+                    for tweet in df2["tweets"]:
+                        for j in tweet.split():
+                            words.append(j)
+                    freq = Counter(words).most_common(30)
+                    freq = pd.DataFrame(freq)
+                    freq.columns = ['word', 'frequency']
+                    sns.barplot(y="word", x="frequency",data=freq)
+                    st.pyplot()
+                else:
+                    st.error("No tweets from the user to Visualize 😕")
 
 
             elif(activity=="Generate Word Cloud"):
-                st.success("Generating WordCloud..")
-                allwords= ' '.join([twt for twt in df2["tweets"]])
-                wordcloud=WordCloud(width=500,height=300,random_state=21,max_font_size=119).generate(allwords)
+                if(len(df)>0):
+                    st.success("Generating WordCloud..")
+                    allwords= ' '.join([twt for twt in df2["tweets"]])
+                    wordcloud=WordCloud(width=500,height=300,random_state=21,max_font_size=119).generate(allwords)
 
-                plt.imshow(wordcloud,interpolation="bilinear")
-                plt.axis("off")
-                st.pyplot(plt.show())
+                    plt.imshow(wordcloud,interpolation="bilinear")
+                    plt.axis("off")
+                    st.pyplot(plt.show())
+                else:
+                    st.error("No tweets from the user to generate 😕")
             else:
                 st.success("Fetching latest tweets")
-                st.markdown("<h2 style='text-align: center;'>Latest Tweets</h2>", unsafe_allow_html=True)
+                
                 posts1=api.user_timeline(screen_name=username,count=10,lang="en",tweet_mode="extended")
                 df3=pd.DataFrame()
                 df3=pd.DataFrame([tweet.full_text for tweet in posts1],columns=['Tweets'])
-                df3['Tweets']=df3['Tweets'].apply(cleanedTweet)
-                j=1
-                for i in df3["Tweets"][:10]:
-                    st.write(str(j)+") "+i)
-                    j+=1
+                def cleanedTweets(tweet):
+                    tweet=re.sub(r"RT[\s]+","",tweet)  # To remove RT(retweet symbol)
+                    tweet=re.sub(r'@[A-Za-z0-9]+','',tweet) # To remove mentions
+                    tweet=re.sub(r'amp',"",tweet)
+                    tweet=re.sub(r'\n'," ",tweet)
+                    tweet = ''.join(i for i in tweet if not i in bad_chars) # To remove special characters
+                    
+                    return tweet
 
+                df3['Tweets']=df3['Tweets'].apply(cleanedTweets)
+                if(len(df3)>0):
+                    st.markdown("<h2 style='text-align: center;'>Latest Tweets</h2>", unsafe_allow_html=True)
+                    j=1
+                    for i in df3["Tweets"][:10]:
+                        st.write(str(j)+") "+i)
+                        j+=1
+                else:
+                    st.error("No tweets from the user 😕")
 if __name__ == "__main__":
 	app()
